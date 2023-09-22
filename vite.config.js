8f9fd202d0ce376af06fc710067f0f7d92f2ca1e
Vakulov <v.vakulov@bftcom.com>
2023-09-23 00:27:19 +0300

    sprint_2

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
-const pageData = {
-  '/index.html': {
-    title: 'Hello!',
-  },
-  '/templates/pages/login/login.html': {
-    title: 'Вход',
-    firstname: 'Виталий'
-  }
-}
-
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
@@ -25,10 +15,6 @@ export default defineConfig({
   plugins: [
     handlebars( {
       root: './src',
-      context(pagePath) {
-        return pageData[pagePath];
-      },
-      partialDirectory: resolve(__dirname, './src/templates/layout'),
     }),
   ],
 })
