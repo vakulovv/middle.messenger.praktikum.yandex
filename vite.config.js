f2819b26fecb60b2d7174991b189dfcab7ed9a4d
Vakulov <v.vakulov@bftcom.com>
2023-09-22 18:46:56 +0300

    sprint_2

--- vite.config.js
+++ vite.config.js
@@ -19,14 +19,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, "/index.html"),
-        login: resolve(__dirname, "/templates/pages/login/login.html"),
-        signup: resolve(__dirname, "/templates/pages/signup/signup.html"),
-        profile: resolve(__dirname, "/templates/pages/profile/profile.html"),
-        profileEdit: resolve(__dirname, "/templates/pages/profile/profileEdit.html"),
-        profileEditPassword: resolve(__dirname, "/templates/pages/profile/profileEditPassword.html"),
-        404: resolve(__dirname, "/templates/pages/error/404.html"),
-        500: resolve(__dirname, "/templates/pages/error/500.html"),
-        chats: resolve(__dirname, "/templates/pages/chats/empty.html"),
       }
     }
   },
